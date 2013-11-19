dd7be962bcb86ede0ca95d072a1931c7e5318a82
vasile <hsctm@rdslink.ro>
2013-11-19 19:34:47 +0200

    ada

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tst::Application.config.secret_key_base = '9b9e5777e27d709fac0c756f5829407bc9907c0d74cfd955a3d4b777ddf31b02a3ff5cef3257ea5e72c5742d2d895acd2a61130c80b6ec2fb60884661c0f823b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tst::Application.config.session_store :cookie_store, key: '_tst_session'
